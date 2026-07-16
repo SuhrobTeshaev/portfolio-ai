2ac456dc6d7aee46858a7d41b8f3ed93f2ce817d
Suhrob Teshaev <suhrobtesha@gmail.com>
2026-07-16 14:45:52 +0600

    Updates

--- assets/CertificatesSection-DhmJ5DWb.js
+++ assets/CertificatesSection-DhmJ5DWb.js
@@ -1,4 +1,4 @@
-import{c as x,u as w,r as c,p as d,j as e,m,B as N,b}from"./index-D_JzAJmY.js";/**
+import{c as x,u as w,r as c,p as d,j as e,m,B as N,b}from"./index-B9WfWR-R.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/EducationLanguagesSection-DWQoJXkK.js
+++ assets/EducationLanguagesSection-DWQoJXkK.js
@@ -1,4 +1,4 @@
-import{c as l,u as r,j as e,m as t,p as c,b as o}from"./index-D_JzAJmY.js";/**
+import{c as l,u as r,j as e,m as t,p as c,b as o}from"./index-B9WfWR-R.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
