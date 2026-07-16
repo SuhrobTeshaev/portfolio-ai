30ddb84ffb197c907e620707e9ba2d4354a10343
Suhrob Teshaev <suhrobtesha@gmail.com>
2026-07-16 13:07:01 +0600

    Updates

--- assets/EducationLanguagesSection-BNviqBHq.js
+++ assets/EducationLanguagesSection-BNviqBHq.js
@@ -1,4 +1,4 @@
-import{c as l,u as r,j as e,m as t,p as c,b as o}from"./index-BLZOmJNc.js";/**
+import{c as l,u as r,j as e,m as t,p as c,b as o}from"./index-SRS3Naeo.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/SkillsSection-D6ppuz6X.js
+++ assets/SkillsSection-D6ppuz6X.js
@@ -1,4 +1,4 @@
-import{c as n,u as r,j as s,m as i,p as m,b as o}from"./index-BLZOmJNc.js";/**
+import{c as n,u as r,j as s,m as i,p as m,b as o}from"./index-SRS3Naeo.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
