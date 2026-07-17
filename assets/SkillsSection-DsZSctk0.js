13f5d8821139f5cbc077210e27286b6e94ec40ed
Suhrob Teshaev <suhrobtesha@gmail.com>
2026-07-17 15:49:19 +0600

    Updates

--- assets/SkillsSection-DsZSctk0.js
+++ assets/SkillsSection-DsZSctk0.js
@@ -1,4 +1,4 @@
-import{c as n,u as r,j as s,m as i,p as m,b as o}from"./index-D8CLy44Z.js";/**
+import{c as n,u as r,j as s,m as i,p as m,b as o}from"./index-D49OHhlO.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
